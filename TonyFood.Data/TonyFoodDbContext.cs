1d9f528e71295400ce9097c309c413f71ea39166
tony evans <stonybonytony@gmail.com>
2019-04-15 16:44:49 +0100

    Added SQLServer support

---
+++ TonyFood.Data/TonyFoodDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TonyFood.Core;
+
+namespace TonyFood.Data
+{
+    public class TonyFoodDbContext : DbContext
+    {
+        public TonyFoodDbContext(DbContextOptions<TonyFoodDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
--- TonyFood/Startup.cs
+++ TonyFood/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TonyFood.Data;
@@ -26,7 +27,14 @@ namespace TonyFood
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
+            services.AddDbContextPool<TonyFoodDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("TonyFood"));
+            });
+
+            services.AddScoped<IRestaurantData, SqlRestaurantData>();
+
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
